80c6f51df2bbed34f0ca3f78bea33ffa4466966c
moreyLee <764457208@qq.com>
2024-05-07 16:20:33 +0400

    新增域名

--- api/v1/system/test.go
+++ api/v1/system/test.go
@@ -12,12 +12,14 @@ func (b *BaseApi) TestT(c *gin.Context) {
 	fmt.Println("执行命令")
 }
 func (b *BaseApi) Domain(c *gin.Context) {
+	shortUrl := c.PostForm("short_url")
+	//invite_url := c.PostForm("invite_url")
 	//account_id :=ce7ca80686b3787313165855f53c401e
-	//CfApiLogin := "djpt36@163.com"
+	CfApiLogin := "djpt36@163.com"
 	//domain := "ss36.vip"
-	//globalKey := "0237bd44ec3b541e622d6aa1b187aac9193f0"
+	globalKey := "0237bd44ec3b541e622d6aa1b187aac9193f0"
 	//zone_id := "f09f2f527f41da9b5f2c100c4ff61fe9"
-	commandStr := "curl -X POST -H \"X-Auth-Key:0237bd44ec3b541e622d6aa1b187aac9193f0\" -H \"X-Auth-Email:djpt36@163.com\" -H \"Content-Type: application/json\" \"https://api.cloudflare.com/client/v4/zones\" --data '{\"name\":\"ss36.vip\",\"jump_start\":\"true\"}'"
+	commandStr := "curl -X POST -H \"X-Auth-Key:\"" + globalKey + "\" -H \"X-Auth-Email:\"" + CfApiLogin + "\" -H \"Content-Type: application/json\" \"https://api.cloudflare.com/client/v4/zones\" --data '{\"name\":\"" + shortUrl + "\",\"jump_start\":\"true\"}'"
 	// 创建域名
 	//curl -X POST -H "X-Auth-Key:0237bd44ec3b541e622d6aa1b187aac9193f0" -H "X-Auth-Email:djpt36@163.com" -H "Content-Type: application/json" "https://api.cloudflare.com/client/v4/zones" --data '{"name":"ss36.vip","jump_start":"true"}'
 	//cmd := exec.Command("pwd")
@@ -26,12 +28,12 @@ func (b *BaseApi) Domain(c *gin.Context) {
 	// 执行命令 并返回输出 获取执行结果
 	output, err := cmd.Output()
 	if err != nil {
-		response.FailWithMessage("命令报错信息:"+err.Error(), c)
+		response.FailWithMessage(err.Error(), c)
 		fmt.Println("命令报错信息:", err.Error())
 		return
 	}
 	// 将命令输出转换为字符串并返回
-	response.OkWithMessage("命令正确执行结果:"+string(output), c)
+	response.OkWithMessage(string(output), c)
 	fmt.Println("命令正确执行结果:", string(output))
 	return
 }
